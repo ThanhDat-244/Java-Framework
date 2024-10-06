e7e447a0cb6358776a1a044729afa3ba59ae5a08
ThanhDat-244 <datlt244@gmail.com>
2024-10-06 09:27:47 +0700

    Delete src/main/java directory

--- src/main/java/SpringFramework/config/VehicleConfig.java
+++
@@ -1,9 +0,0 @@
-package SpringFramework.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = "SpringFramework.entities")
-public class VehicleConfig {
-}
