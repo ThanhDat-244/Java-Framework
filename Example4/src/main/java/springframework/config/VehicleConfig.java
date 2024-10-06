11b9098ddb3616198023e7583e18bfd7041081f1
ThanhDat-244 <datlt244@gmail.com>
2024-10-06 09:35:45 +0700

    Creating Beans programmatically using registerBean

---
+++ Example4/src/main/java/springframework/config/VehicleConfig.java
@@ -0,0 +1,10 @@
+package springframework.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "springframework.entities")
+public class VehicleConfig {
+
+}
